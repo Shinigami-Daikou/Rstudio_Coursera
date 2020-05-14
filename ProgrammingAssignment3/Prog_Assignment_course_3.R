0cb60822f18a832d1cc24571bf890e9c68504d65
Bhupendra Chauhan <54802156+Shinigami-Daikou@users.noreply.github.com>
2020-05-15 04:13:41 +0530

    Update Prog_Assignment_course_3.R

--- ProgrammingAssignment3/Prog_Assignment_course_3.R
+++ ProgrammingAssignment3/Prog_Assignment_course_3.R
@@ -9,15 +9,16 @@ unzip('UCI HAR Dataset.zip')
 
 
 ##Load the datasets
-activity_labels<-read.table('./UCI HAR Dataset/activity_labels.txt')
-features<-read.table('./UCI HAR Dataset/features.txt')
+##But the data has to be in your Working Directory
+activity_labels<-read.table('./activity_labels.txt')
+features<-read.table('./features.txt')
 features$V2<-gsub("\\()","",features$V2)
-subject_train<-read.table('./UCI HAR Dataset/train/subject_train.txt')
-X_train<-read.table('./UCI HAR Dataset/train/X_train.txt')
-y_train<-read.table('./UCI HAR Dataset/train/y_train.txt')
-subject_test<-read.table('./UCI HAR Dataset/test/subject_test.txt')
-X_test<-read.table('./UCI HAR Dataset/test/X_test.txt')
-y_test<-read.table('./UCI HAR Dataset/test/y_test.txt')
+subject_train<-read.table('./train/subject_train.txt')
+X_train<-read.table('./train/X_train.txt')
+y_train<-read.table('./train/y_train.txt')
+subject_test<-read.table('./test/subject_test.txt')
+X_test<-read.table('./test/X_test.txt')
+y_test<-read.table('./test/y_test.txt')
 
 
 
